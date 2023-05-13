89a97d0dff16c25f2640366cb1c0624f05aca243
Yakiyo <yakiyo1206@gmail.com>
2023-05-13 07:48:08 +0000

    feat: add modules and impl empty funcs for version

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,9 @@
-pub mod channel;
+#![doc = include_str!("../README.md")]
 
-use channel::*;
+mod channel;
+mod parser;
+
+pub use channel::*;
 
 pub struct Version {
     pub channel: Channel,
@@ -9,4 +12,17 @@ pub struct Version {
     pub patch: usize,
     pub prerelease: Option<usize>,
     pub prerelease_patch: Option<usize>,
+}
+
+impl Version {
+    fn _parse<P: AsRef<str>>(_s: P) {}
+}
+
+impl std::str::FromStr for Version {
+    type Err = &'static str;
+
+    fn from_str(_s: &str) -> Result<Self, Self::Err> {
+        
+        Err("Invalid format")
+    }
 }
