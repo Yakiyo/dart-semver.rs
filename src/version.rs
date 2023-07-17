af92c4d187b57bb4e0b04f858089a1fdabac7786
Yakiyo <yakiyo1206@gmail.com>
2023-07-17 04:31:18 +0000

    feat: derives

--- src/version.rs
+++ src/version.rs
@@ -1,25 +1,36 @@
 use crate::channel::Channel;
 
+/// An enum of possible version formats
+#[derive(Debug, PartialEq, Clone, Copy)]
 pub enum Version {
+    /// Major, Minor, patch - v2.3.4
     FullStable(usize, usize, usize),
+    /// Major, minor - v3.1
     MajorMinor(usize, usize),
+    /// Major v3
     MajorOnly(usize),
+    /// Major, Minor, Patch, Prerelease, Prerelease-patch, Channel - v3.1.2-4.5.beta
     NonStable(usize, usize, usize, usize, usize, Channel),
 }
 
 impl Version {
     /// If version is stable or not
     pub fn is_stable(&self) -> bool {
-        match self {
-            Self::NonStable(..) => false,
-            _ => true,
+        if matches!(self, Self::NonStable(..)) {
+            false
+        } else {
+            true
         }
+        // match self {
+        //     Self::NonStable(..) => false,
+        //     _ => true,
+        // }
     }
 
     /// Returns the channel of the version string
     pub fn channel(&self) -> Channel {
         match self {
-            Self::NonStable(.., c) => c.clone(),
+            Self::NonStable(.., c) => *c,
             _ => Channel::Stable,
         }
     }
